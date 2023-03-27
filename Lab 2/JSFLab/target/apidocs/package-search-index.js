6f8064afc600393a0c4b5c2dfcf4d0bcfeaa1640
Mateusz Kozłowicz <69522734+Koziok@users.noreply.github.com>
2023-03-27 21:20:03 +0200

    Add files via upload

---
+++ Lab 2/JSFLab/target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.mycompany.JSFLab"},{"l":"com.mycompany.JSFLab.resources"},{"l":"view.backing"}]
---
+++ Lab 2/JSFLab/target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.mycompany.JSFLab.resources","l":"JakartaEE10Resource"},{"p":"com.mycompany.JSFLab","l":"JakartaRestConfiguration"},{"p":"view.backing","l":"LoginBean"}]
