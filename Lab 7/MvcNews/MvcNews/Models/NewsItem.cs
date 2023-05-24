3c72308f553d3f289234be7568a9a2c19aaf6a87
Mateusz Kozłowicz <mati-2208@wp.pl>
2023-05-24 20:20:28 +0200

    Add lab 7

---
+++ Lab 7/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+        [Required(ErrorMessage = "Field required")]
+        [StringLength(140, MinimumLength = 5, ErrorMessage = "Required 5-140 characters")]
+        public string Text { get; set; } = string.Empty;
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
